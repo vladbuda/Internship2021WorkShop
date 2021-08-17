c357e681c69a9bf39d407feb0fc1690b6476043b
Vlad-Cristian Buda <34052906+vladbuda@users.noreply.github.com>
2021-08-17 11:12:26 +0300

    initial commit; smartphone service readu

---
+++ TechShop.Domain/Models/SmartPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShop.Domain.Models
+{
+    public class SmartPhone
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Storage { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ TechShop.Services/SmartPhoneService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechShop.Domain.Models;
+
+namespace TechShop.Services
+{
+    public class SmartPhoneService
+    {
+        private List<SmartPhone> _smartPhones = new List<SmartPhone>()
+        {
+            new SmartPhone()
+            {
+                Id = 1,
+                Brand = "Samsung",
+                Model = "Galaxy S21",
+                Storage = 128,
+                Price = 799
+            },
+            new SmartPhone()
+            {
+                Id = 2,
+                Brand = "Apple",
+                Model = "iPhone 12",
+                Storage = 64,
+                Price = 699
+            },
+            new SmartPhone()
+            {
+                Id = 3,
+                Brand = "Google",
+                Model = "Pixel 5",
+                Storage = 128,
+                Price = 599
+            }
+        };
+
+        public List<SmartPhone> GetSmartPhones()
+        {
+            return _smartPhones;
+        }
+
+        public SmartPhone GetSmartPhone(int id)
+        {
+            var smartPhone = _smartPhones.FirstOrDefault(smartPhone => smartPhone.Id == id);
+
+            return smartPhone;
+        }
+    }
+}
